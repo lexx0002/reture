fe2159eb907577f24d0791e222ea568ec4d5e750
lexx0002 <33981077+lexx0002@users.noreply.github.com>
2019-06-05 10:17:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 
-
 class SimpleIterator:
     def __iter__(self):
         return self
@@ -11,20 +10,28 @@ class SimpleIterator:
             file = json.load(file)
         self.file = file
         self.counter = -1
-        self.last_line = len(file)
+        self.last_line = len(file) - 1
+        self.file_is_created = False
 
     def __next__(self):
-        if self.counter != self.last_line:
+        if self.counter < self.last_line:
             self.counter += 1
-            return self.file[self.counter]
         else:
             raise StopIteration
-        self.counter += 1
+        self.country = self.file[self.counter]
+        self.country_name = self.country['name']['common']
+        self.rus_name = self.country['translations']['rus']['common']
+        self.country_link = self.country_name + ' - https://ru.wikipedia.org/wiki/' + self.rus_name
+        if self.file_is_created == False:
+            with open('new_file.txt', 'w', encoding='utf8') as new_file:
+                new_file.write(self.country_link)
+                new_file.write('"\n')
+            self.file_is_created = True
+        else:
+            with open('new_file.txt', 'a', encoding='utf8') as new_file:
+                new_file.write(self.country_link)
+                new_file.write('"\n')
+        return self.rus_name + ' Записана'
 
-
-
-
-s_iter2 = SimpleIterator('countries.json')
-print(s_iter2)
-print(s_iter2.__next__())
-print(s_iter2.__next__())
+for item in SimpleIterator('countries.json'):
+    print(item)
